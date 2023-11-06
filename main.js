80d91e81e76e8b30071d773646e9ed74298aa802
hallstrom91 <143492796+hallstrom91@users.noreply.github.com>
2023-11-06 11:56:34 +0100

    update

--- main.js
+++ main.js
@@ -1,63 +1,80 @@
 "strict";
 
-// Buttons
-addButton = document.getElementById("add-btn");
-clrButton = document.getElementById("clr-btn");
-const editMode = false;
-
-// Information from Input Fields
+// Setup WopWOp
+const addButton = document.getElementById("add-btn");
+const clrButton = document.getElementById("clr-btn");
 const fullName = document.getElementById("fullName");
 const phone = document.getElementById("phone");
-
-// Store saved contacts
 const saved = document.getElementById("saved");
+let editMode = false;
 
-// Add Contact Button
-addButton.addEventListener("click", function () {
-  // fetch name and number
-  const name = fullName.value;
-  const number = phone.value;
-  // create locked inputfield for name
-  const savedName = document.createElement("input");
-  savedName.type = "text";
-  savedName.value = name;
-  savedName.disabled = true;
-  savedName.classList.add("newInput");
-  // create locked inputfield for phone
-  const savedPhone = document.createElement("input");
-  savedPhone.labels = "Telefon";
-  savedPhone.type = "tel";
-  savedPhone.value = number;
-  savedPhone.disabled = true;
-  savedPhone.classList.add("newInput");
-  // create a new storage div for saved inputfields and info
-  const savedContacts = document.createElement("div");
-  savedContacts.appendChild(savedName);
-  savedContacts.appendChild(savedPhone);
-  saved.appendChild(savedContacts);
-  // create button for edit new locked inputfields
-  const editButton = document.createElement("newBtn");
-  editButton.textContent = editMode ? "Uppdatera" : "Ändra";
-  editButton.classList.add("button");
-  editButton.addEventListener("click", function () {
-    if (editMode) {
-      savedName.value = fullName.value;
-      savedPhone.value = phone.value;
-    }
-    editMode = !editMode;
-    savedName.disabled = !editMode;
-    savedPhone.disabled = !editMode;
-    editButton.textContent = editMode ? "Uppdatera" : "Ändra";
+function changeValue(button, fullName, phone) {
+  editMode = !editMode;
+  button.textContent = editMode ? "Spara" : "Ändra";
+  fullName.disabled = !editMode;
+  phone.disabled = !editMode;
+}
+
+// Clear Contact-Fields Button  (unsaved)
+function clearContactFields() {
+  clrButton.addEventListener("click", function () {
+    fullName.value = "";
+    phone.value = "";
   });
-  saved.appendChild(savedContacts);
-  savedContacts.appendChild;
-  // clear fields after input save
-  fullName.value = "";
-  phone.value = "";
-});
+}
 
-// Clear Contact-Fields Button
-clrButton.addEventListener("click", function () {
-  fullName.value = "";
-  phone.value = "";
-});
+function addContactNow() {
+  // Add New Contact w buttton-click
+  addButton.addEventListener("click", function () {
+    // Fetch and assign new name and number
+    const name = fullName.value;
+    const number = phone.value;
+
+    // Create locked inputfield for name
+    const savedName = document.createElement("input");
+    savedName.type = "text";
+    savedName.value = name;
+    savedName.disabled = !editMode;
+    savedName.classList.add("newInput");
+
+    // Create locked inputfield for phone
+    const savedPhone = document.createElement("input");
+    savedPhone.type = "tel";
+    savedPhone.value = number;
+    savedPhone.disabled = !editMode;
+    savedPhone.classList.add("newInput");
+
+    // Create a new storage div for saved inputfields and info
+    const savedContacts = document.createElement("div");
+    savedContacts.appendChild(savedName);
+    savedContacts.appendChild(savedPhone);
+    saved.appendChild(savedContacts);
+
+    // Edit button for saved contacts
+    const changeButton = document.createElement("button");
+    changeButton.textContent = "Ändra";
+    changeButton.classList.add("newBtn");
+    changeButton.addEventListener("click", function () {
+      changeValue(changeButton, savedName, savedPhone);
+    });
+    // Save button for edited (saved) contacts
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Spara";
+    saveButton.classList.add("newBtn");
+    saveButton.style.display = "none";
+
+    saveButton.addEventListener("click", function () {
+      savedName.value = fullName.value;
+      savedPhone = phone.value;
+      changeValue(changeButton, savedName, savedPhone);
+      saveButton.style.display = "none";
+      changeButton.style.display = "inline-block";
+    });
+    saved.appendChild(changeButton);
+    saved.appendChild(saveButton);
+
+    // Clear fields after input save
+    fullName.value = "";
+    phone.value = "";
+  });
+}
