08796e68e21eaa82dc4aceada6801c79b35543e1
Simon Hällström <143492796+hallstrom91@users.noreply.github.com>
2023-11-08 08:29:07 +0100

    update w errors2

--- main.js
+++ main.js
@@ -8,7 +8,35 @@ const phone = document.getElementById("phone");
 const saved = document.getElementById("saved");
 let editMode = false;
 
-// FUCKING SHIT FUCK FUCK
+function createEditBtn(savedName, savedPhone) {
+  // Edit button for saved contacts
+  let changeButton = document.createElement("button");
+  changeButton.textContent = "Ändra";
+  changeButton.classList.add("newBtn");
+  // Save button for edited (saved) contacts
+  let saveButton = document.createElement("button");
+  saveButton.textContent = "Spara";
+  saveButton.classList.add("newBtn");
+  saveButton.style.display = "none";
+  // Listen for click on Edit/Change button
+  changeButton.addEventListener("click", function () {
+    changeValue(saveButton, savedName, savedPhone);
+    saveButton.style.display = "inline-block";
+    changeButton.style.display = "none";
+  });
+  // Listen for click on Save button
+  saveButton.addEventListener("click", function () {
+    savedName.value = fullName.value;
+    savedPhone.value = phone.value;
+    changeValue(changeButton, savedName, savedPhone);
+    saveButton.style.display = "none";
+    changeButton.style.display = "inline-block";
+  });
+  saved.appendChild(changeButton);
+  return [changeButton, saveButton];
+}
+
+// Change Value in saved Inputfields Function
 function changeValue(button, fullName, phone) {
   editMode = !editMode;
   button.textContent = editMode ? "Spara" : "Ändra";
@@ -16,83 +44,66 @@ function changeValue(button, fullName, phone) {
   phone.disabled = !editMode;
 }
 
-function createEditBtn(name, phone) {
-  // Edit button for saved contacts
-  let changeButton = document.createElement("button");
-  changeButton.textContent = "Ändra";
-  changeButton.classList.add("newBtn");
+// Listen for "click" and add contact
+addButton.addEventListener("click", addContactNow);
 
-  // Save button for edited (saved) contacts
-  let saveButton = document.createElement("button");
-  saveButton.textContent = "Spara";
-  saveButton.classList.add("newBtn");
-  saveButton.style.display = "none";
-
-  // Listen for click on Edit/Change button
-  changeButton.addEventListener("click", function () {
-    changeValue(changeButton, name, phone);
-    saveButton.style.display = "inline-block";
-    changeButton.style.display = none;
-  });
-  // Listen for click on Save button
-  saveButton.addEventListener("click", function () {
-    savedName.value = fullName.value;
-    savedPhone.value = phone.value;
-    changeValue(changeButton, name, phone);
-    saveButton.style.display = "none";
-    changeButton.style.display = "inline-block";
-  });
-  saved.appendChild(changeButton);
-  saved.appendChild(saveButton);
-
-  return changeButton, saveButton;
-}
-
-// FUNCTION TO ADD FUCKING CONTACTS
+// Add contacts function
 function addContactNow() {
-  // Add New Contact w buttton-click
-  addButton.addEventListener("click", function () {
-    // Fetch and assign new name and number
-    const name = fullName.value;
-    const number = phone.value;
-
-    // Create locked inputfield for name
-    const savedName = document.createElement("input");
-    savedName.type = "text";
-    savedName.value = name;
-    savedName.disabled = !editMode;
-    savedName.classList.add("newInput");
-
-    // Create locked inputfield for phone
-    const savedPhone = document.createElement("input");
-    savedPhone.type = "tel";
-    savedPhone.value = number;
-    savedPhone.disabled = !editMode;
-    savedPhone.classList.add("newInput");
-
-    // Create a new storage div for saved inputfields and info
-    const savedContacts = document.createElement("div");
-    savedContacts.appendChild(savedName);
-    savedContacts.appendChild(savedPhone);
-    saved.appendChild(savedContacts);
-
-    // Call edit function N buttons
-    let [changeButton, saveButton] = createEditBtn(name, phone);
-
-    /* saved.appendChild(changeButton);
-    saved.appendChild(saveButton);
- */
-    fullName.value = "";
-    phone.value = "";
-  });
+  // Fetch and assign new name and number
+  const name = fullName.value;
+  const number = phone.value;
+  // Create locked inputfield for name
+  const savedName = document.createElement("input");
+  savedName.type = "text";
+  savedName.value = name;
+  savedName.disabled = !editMode;
+  savedName.classList.add("newInput");
+  // Create locked inputfield for phone
+  const savedPhone = document.createElement("input");
+  savedPhone.type = "tel";
+  savedPhone.value = number;
+  savedPhone.disabled = !editMode;
+  savedPhone.classList.add("newInput");
+  // Create a new storage div for saved inputfields and info
+  const savedContacts = document.createElement("div");
+  savedContacts.appendChild(savedName);
+  savedContacts.appendChild(savedPhone);
+  saved.appendChild(savedContacts);
+  // Call edit function N buttons
+  let [changeButton, saveButton] = createEditBtn(savedName, savedPhone);
+  let removeButton = createRemoveBtn(savedName, savedPhone, savedContacts);
+  savedContacts.appendChild(changeButton);
+  savedContacts.appendChild(saveButton);
+  savedContacts.appendChild(removeButton);
+  // Clear Static inputfields after adding to dynamic
+  fullName.value = "";
+  phone.value = "";
 }
 
 // Clear Contact-Fields Button (unsaved inputfields)
+clrButton.addEventListener("click", clearContactFields);
+
 function clearContactFields() {
-  clrButton.addEventListener("click", function () {
-    fullName.value = "";
-    phone.value = "";
+  fullName.value = "";
+  phone.value = "";
+}
+
+// Remove saved contacts function
+function createRemoveBtn(savedName, savedPhone, savedContacts) {
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Delete";
+  removeButton.classList.add("newBtn");
+
+  removeButton.addEventListener("click", function () {
+    savedContacts.removeChild(savedName);
+    savedContacts.removeChild(savedPhone);
+    savedContacts.removeChild(removeButton);
   });
+  return removeButton;
+}
+
+// error messages
+if (fullName === "" && phone === "" && editmode) {
 }
 
 // UNDER NEW STORAGE DIV IF CODE BREAKS
